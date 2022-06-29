be2b30fe1880df866121568f6485f15744676db5
Esteban Barthe <estebanbarthe99@gmail.com>
2022-06-29 16:47:37 -0300

    Se actualiza la clase 2022 06 27

---
+++ PHP/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    $numero = 15;
+
+    $texto = "Soy un texto";
+    var_dump($texto);
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Soy html</h1>
+        <?php echo("<h1>Hola soy PHP</h1>")     ?>
+    </body>
+</html>
+
